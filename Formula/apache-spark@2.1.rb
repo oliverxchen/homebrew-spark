ff1923d0ff5e01a54867e2bd1489e1177b8280ba
oliverxchen <oliverxchen@gmail.com>
2017-08-23 00:35:29 +0800

    tap for version 2.1.1

---
+++ Formula/apache-spark@2.1.rb
@@ -0,0 +1,24 @@
+class ApacheSparkAT21 < Formula
+  desc "Engine for large-scale data processing"
+  homepage "https://spark.apache.org"
+  url "https://d3kbcqa49mib13.cloudfront.net/spark-2.1.1-bin-hadoop2.7.tgz"
+  version "2.1.1"
+  sha256 "372ac4f73221c07696793101007a4f19e31566d1f0d9bd0e5205b6fb5b45bfc2"
+
+  bottle :unneeded
+
+  keg_only :versioned_formula
+
+  def install
+    # Rename beeline to distinguish it from hive's beeline
+    mv "bin/beeline", "bin/spark-beeline"
+
+    rm_f Dir["bin/*.cmd"]
+    libexec.install Dir["*"]
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match "Long = 1000", pipe_output(bin/"spark-shell", "sc.parallelize(1 to 1000).count()")
+  end
+end
